700c1641fe99d54991606ddc09b4b62af0bc9d96
lvliuzhong <lvliuzhong@wondertek.com.cn>
2016-11-18 10:33:21 +0800

    接口测试

--- src/main/java/com/wondertek/mobilevideo/recommend/webapp/action/TestAction.java
+++ src/main/java/com/wondertek/mobilevideo/recommend/webapp/action/TestAction.java
@@ -1,24 +1,21 @@
 package com.wondertek.mobilevideo.recommend.webapp.action;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.httpclient.NameValuePair;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.wondertek.mobilevideo.core.recommend.cache.redis.service.UserTagCacheManager;
 import com.wondertek.mobilevideo.core.recommend.search.HttpClientUtil;
 import com.wondertek.mobilevideo.core.recommend.util.RequestConstants;
-import com.wondertek.mobilevideo.core.recommend.vo.mongo.CatInfo;
 import com.wondertek.mobilevideo.core.recommend.vo.mongo.UserTag;
 import com.wondertek.mobilevideo.core.util.StringUtil;
 
 /**
- * 
  * @author madongwei
  * 处理testAction
- *
  */
 public class TestAction extends BaseAction {
 	private static final long serialVersionUID = -2836437294838899724L;
@@ -26,41 +23,40 @@ public class TestAction extends BaseAction {
 	private UserTagCacheManager userTagCacheManager;
 	private UserTag userTag;
 	private String catsInfoJson; //这个是默认模式传来的catsJson
-	private String catsJson;  //这是自定义格式传来的json
-	private int choosJson; //这是选择哪一种模式传json   value = 1 表示是第一种默认模式，，，value=2 表示是自定义模式传json
-	
-	
+	/**
+	 * 测试获取用户标签
+	 */
 	public void testQueryTag() {
 		try {
-			resultMap.put("success", true);
+			//获取参数
 			String userId = getParam("userId");
 			String host = getParam("host");
+			//校验参数
 			if (StringUtil.isNullStr(userId)) {
 				resultMap.put(RequestConstants.R_SUCC, Boolean.FALSE);
-				resultMap.put(RequestConstants.R_CODE, RequestConstants.R_CODE_110001);
-				resultMap.put(RequestConstants.R_MSG, "userId为空");
+				resultMap.put(RequestConstants.R_MSG, "用户ID不可为空");
 				this.writeTextResponse(JSON.toJSONString(resultMap));
 				return ;
 			}
 			if (StringUtil.isNullStr(host)) {
-				resultMap.put(RequestConstants.R_CODE, RequestConstants.R_CODE_110003);
 				resultMap.put(RequestConstants.R_SUCC, Boolean.FALSE);
-				resultMap.put(RequestConstants.R_MSG,this.getText("request.error.contentnull"));
+				resultMap.put(RequestConstants.R_MSG, "服务器不可为空");
 				this.writeTextResponse(JSON.toJSONString(resultMap));
 				return ;
 			}
+			//请求指定推荐服务器
 			String url = "http://" + host + "/req/queryTag.msp";
-			//可以传递
+			
 			Map<String, String> map = new HashMap<String,String>();
 			map.put("userId", userId);
 			String rst = HttpClientUtil.httpClientPost(url, HttpClientUtil.praseParameterMap(map));
 			if(StringUtil.isNullStr(rst)){
-				resultMap.put(RequestConstants.R_CODE, RequestConstants.R_CODE_110003);
 				resultMap.put(RequestConstants.R_SUCC, Boolean.FALSE);
-				resultMap.put(RequestConstants.R_MSG,"连接服务器出现异常");
+				resultMap.put(RequestConstants.R_MSG,"连接指定推荐服务器出现异常");
 				this.writeTextResponse(JSON.toJSONString(resultMap));
 				return;
 			}
+			//解析返回数据
 			JSONObject obj = JSON.parseObject(rst);
 			if(obj.containsKey("root") && obj.containsKey("success") && obj.getBooleanValue("success") 
 					&& obj.containsKey("returnCode") && "000000".equals(obj.getString("returnCode"))
@@ -80,30 +76,30 @@ public class TestAction extends BaseAction {
 			this.writeTextResponse(JSON.toJSONString(resultMap));
 		}
 		return ;
-		
 	}
 	/**
-	 * 处理testAction
-	 *  [{"catName":"电影","score":1.03,"recommendation":[{"label":"影视1","score":100}],"items":[{"labelName":"播出年代","labelValue":"2016","score":100}]},{"catName":"电视剧","score":1.03,"recommendation":[{"label":"影视2","score":100}],"items":[{"labelName":"播出年代","labelValue":"2016","score":100}]}]}
-	 * @return
+	 * 测试内容推荐
 	 */
-	//userTag的json {"id":"681274129","prdType":"MIGUVIDEO","start":"0","limit":"10","ctVer":"v1.0","cats":[{"catName":"电影","score":1.03,"recommendation":[{"label":"影视1","score":100}],"items":[{"labelName":"播出年代","labelValue":"2016","score":100}]},{"catName":"电视剧","score":1.03,"recommendation":[{"label":"影视2","score":100}],"items":[{"labelName":"播出年代","labelValue":"2016","score":100}]}]
-	
-	
 	public void testSearch() {
-		String host = getParam("host");
-		resultMap.put(RequestConstants.R_SUCC, Boolean.TRUE);
 		try {
+			//获取校验参数
+			String host = getParam("host");
+			if (StringUtil.isNullStr(host)) {
+				resultMap.put(RequestConstants.R_SUCC, Boolean.FALSE);
+				resultMap.put(RequestConstants.R_MSG, "服务器不可为空");
+				this.writeTextResponse(JSON.toJSONString(resultMap));
+				return ;
+			}
 			catsInfoJson = StringUtil.null2Str(catsInfoJson);
 			UserTag oldUserTag = null;
 			if(StringUtil.isNullStr(catsInfoJson)) {
 				try {
 					oldUserTag = JSON.parseObject(catsInfoJson, UserTag.class);
 				} catch (Exception e) {
-					
+					log.error("处理json串出错，msg:"+e.getMessage()+",json:" + catsInfoJson);
 				}
 			}
-			if(oldUserTag != null){
+			if(oldUserTag != null){//以手动输入的为准
 				if(!StringUtil.isNullStr(userTag.getId())){
 					oldUserTag.setId(userTag.getId());
 				}
@@ -119,17 +115,22 @@ public class TestAction extends BaseAction {
 				if(!StringUtil.isNullStr(userTag.getPrdType())){
 					oldUserTag.setPrdType(userTag.getPrdType());
 				}
-			}else{
+			}else{//否则默认就是输入的对象
 				oldUserTag = userTag;
+			}
+			if(oldUserTag == null){
+				resultMap.put(RequestConstants.R_SUCC, Boolean.FALSE);
+				resultMap.put(RequestConstants.R_MSG,"未找到任何参数");
+				this.writeTextResponse(JSON.toJSONString(resultMap));
+				return;
 			}
 		    String userTagJson = JSON.toJSONString(oldUserTag);
 		    //可以传递
 			String url = "http://" + host + "/req/search.msp";
 			String rst = HttpClientUtil.httpClientPost(url, new NameValuePair[0], userTagJson);
 			if(StringUtil.isNullStr(rst)){
-				resultMap.put(RequestConstants.R_CODE, RequestConstants.R_CODE_110003);
 				resultMap.put(RequestConstants.R_SUCC, Boolean.FALSE);
-				resultMap.put(RequestConstants.R_MSG,"连接服务器出现异常");
+				resultMap.put(RequestConstants.R_MSG,"连接指定推荐服务器出现异常");
 				this.writeTextResponse(JSON.toJSONString(resultMap));
 				return;
 			}
@@ -138,12 +139,7 @@ public class TestAction extends BaseAction {
 			resultMap.put("error", true);
 			this.writeTextResponse(JSON.toJSONString(resultMap));
 		}
-		return ;
-		
-		
 	}
-
-	
 	
 	public UserTagCacheManager getUserTagCacheManager() {
 		return userTagCacheManager;
@@ -163,22 +159,4 @@ public class TestAction extends BaseAction {
 	public void setCatsInfoJson(String catsInfoJson) {
 		this.catsInfoJson = catsInfoJson;
 	}
-	public String getCatsJson() {
-		return catsJson;
-	}
-	public void setCatsJson(String catsJson) {
-		this.catsJson = catsJson;
-	}
-	public int getChoosJson() {
-		return choosJson;
-	}
-	public void setChoosJson(int choosJson) {
-		this.choosJson = choosJson;
-	}
-	
-	
 }
-	
-	
-
-
