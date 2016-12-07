4861ef3b0d24e9cfea8c381cb069d01c5be5562a
madongwei <1457042581@qq.com>
2016-12-07 10:44:28 +0800

    test

--- src/main/java/com/wondertek/mobilevideo/recommend/webapp/action/TestAction.java
+++ src/main/java/com/wondertek/mobilevideo/recommend/webapp/action/TestAction.java
@@ -1,12 +1,16 @@
 package com.wondertek.mobilevideo.recommend.webapp.action;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.httpclient.NameValuePair;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.wondertek.mobilevideo.core.poms.bean.Poms;
+import com.wondertek.mobilevideo.core.poms.service.PomsService;
 import com.wondertek.mobilevideo.core.recommend.cache.redis.service.UserTagCacheManager;
 import com.wondertek.mobilevideo.core.recommend.util.RequestConstants;
 import com.wondertek.mobilevideo.core.recommend.vo.mongo.UserTag;
@@ -23,6 +27,8 @@ public class TestAction extends BaseAction {
 	private UserTagCacheManager userTagCacheManager;
 	private UserTag userTag;
 	private String catsInfoJson; //这个是默认模式传来的catsJson
+	private PomsService pomsService;
+	
 	/**
 	 * 测试获取用户标签
 	 */
@@ -140,6 +146,103 @@ public class TestAction extends BaseAction {
 			this.writeTextResponse(JSON.toJSONString(resultMap));
 		}
 	}
+
+	/**
+	 * 测试系统由prdContId 得到整个节目的信息
+	 */
+	public void testSystemSearch() {
+		try {
+			//获取校验参数
+			String host = getParam("host");
+			if (StringUtil.isNullStr(host)) {
+				resultMap.put(RequestConstants.R_SUCC, Boolean.FALSE);
+				resultMap.put(RequestConstants.R_MSG, "服务器不可为空");
+				this.writeTextResponse(JSON.toJSONString(resultMap));
+				return ;
+			}
+			catsInfoJson = StringUtil.null2Str(catsInfoJson);
+			UserTag oldUserTag = null;
+			if(!StringUtil.isNullStr(catsInfoJson)) {
+				try {
+					oldUserTag = JSON.parseObject(catsInfoJson, UserTag.class);
+				} catch (Exception e) {
+					log.error("处理json串出错，msg:"+e.getMessage()+",json:" + catsInfoJson);
+				}
+			}
+			if(oldUserTag != null){//以手动输入的为准
+				if(!StringUtil.isNullStr(userTag.getId())){
+					oldUserTag.setId(userTag.getId());
+				}
+				if(userTag.getStart() != null){
+					oldUserTag.setStart(userTag.getStart());
+				}
+				if(userTag.getLimit() != null){
+					oldUserTag.setLimit(userTag.getLimit());
+				}
+				if(!StringUtil.isNullStr(userTag.getCtVer())){
+					oldUserTag.setCtVer(userTag.getCtVer());
+				}
+				if(!StringUtil.isNullStr(userTag.getPrdType())){
+					oldUserTag.setPrdType(userTag.getPrdType());
+				}
+			}else{//否则默认就是输入的对象
+				oldUserTag = userTag;
+			}
+			if(oldUserTag == null){
+				resultMap.put(RequestConstants.R_SUCC, Boolean.FALSE);
+				resultMap.put(RequestConstants.R_MSG,"未找到任何参数");
+				this.writeTextResponse(JSON.toJSONString(resultMap));
+				return;
+			}
+		    String userTagJson = JSON.toJSONString(oldUserTag);
+		    //可以传递
+			String url = "http://" + host + "/req/search.msp";
+			String rst = HttpClientUtil.httpClientPost(url, new NameValuePair[0], userTagJson);
+			
+			if(StringUtil.isNullStr(rst)){
+				resultMap.put(RequestConstants.R_SUCC, Boolean.FALSE);
+				resultMap.put(RequestConstants.R_MSG,"连接指定推荐服务器出现异常");
+				this.writeTextResponse(JSON.toJSONString(resultMap));
+				return;
+			}
+			//根据rootArray得到 List<Poms>
+			JSONObject rstObjJson = JSON.parseObject(rst);
+			JSONArray rootArray = (JSONArray)JSON.parse(rstObjJson.get("root").toString());
+			List<Poms> pomses = this.getPomses(rootArray);
+			System.out.println(pomses);
+			
+			this.writeTextResponse(JSON.toJSONString(JSON.parseObject(rst), true));
+		} catch (Exception e) {
+			resultMap.put("error", true);
+			this.writeTextResponse(JSON.toJSONString(resultMap));
+		}
+	}
+	
+	//根据rootArray去service中得到 List<Poms>
+	public List<Poms> getPomses(JSONArray rootArray ) {
+		List<Long> prdContIds = null;
+		List<Poms> pomses = null;
+		try {
+			if (rootArray != null) {
+				//遍历rootArray
+				String rootArrayStr = JSON.toJSONString(rootArray);
+				System.out.println(rootArrayStr);
+				List<HashMap> rootArrayMap = JSON.parseArray(rootArrayStr, HashMap.class);
+				for (int i = 0; i < rootArrayMap.size(); i++) {
+					String prdContId = (String) rootArrayMap.get(i).get("prdContId");
+					 prdContIds.add(StringUtil.nullToLong(prdContId));
+				}
+			}
+			pomses = pomsService.testSystemSearch(prdContIds);
+			
+		} catch (Exception e) {
+			resultMap.put("error", true);
+			resultMap.put(RequestConstants.R_MSG, "出现未知错误");
+			e.printStackTrace();
+		}
+		return pomses;
+			
+	}
 	
 	public UserTagCacheManager getUserTagCacheManager() {
 		return userTagCacheManager;
@@ -159,4 +262,11 @@ public class TestAction extends BaseAction {
 	public void setCatsInfoJson(String catsInfoJson) {
 		this.catsInfoJson = catsInfoJson;
 	}
+	public PomsService getPomsService() {
+		return pomsService;
+	}
+	public void setPomsService(PomsService pomsService) {
+		this.pomsService = pomsService;
+	}
+	
 }
